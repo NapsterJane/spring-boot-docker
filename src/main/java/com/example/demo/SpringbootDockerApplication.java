5f55b62e00fa19bfc81f0c47e3441122377b5c7c
napster <napster@192.168.29.90>
2024-11-01 17:30:24 +0530

    web contents

--- src/main/java/com/example/demo/SpringbootDockerApplication.java
+++ src/main/java/com/example/demo/SpringbootDockerApplication.java
@@ -2,18 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringbootDockerApplication {
-	
-	@GetMapping("/hello")
-    public String homePage() {
-        return "welcome Docker";
-    }
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerApplication.class, args);
 	}
