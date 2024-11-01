0d114ffac43c7404880bfaaea8bc3e110b75cb1f
apple <apple@192.168.29.90>
2024-11-01 13:44:56 +0530

    commit from branch

--- src/main/java/com/example/demo/SpringbootDockerApplication.java
+++ src/main/java/com/example/demo/SpringbootDockerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringbootDockerApplication {
 	
 	@GetMapping("/hello")
-    public String home() {
+    public String homePage() {
         return "welcome";
     }
 
